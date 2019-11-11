afc79fd935a2a771cd0ea53237608103a0149ed0
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-11-11 13:16:10 +0530

    task1: Add rms error calculation.

--- gradient_decent.py
+++ gradient_decent.py
@@ -1,16 +1,21 @@
 import numpy as np
 import pandas as pd
+import math
 
 data = pd.read_csv('3D_spatial_network.csv')
+N = len(data)
+
+train_set_size = int(0.7 * N)
+test_set_size = int(0.3 * N)
 
 # Equation: y = w1x1 + w2x2 + w0
 
-X1 = data.iloc[0:, 1]
-X2 = data.iloc[0:, 2]
-Y = data.iloc[0:, 3]
+X1 = data.iloc[0: train_set_size, 1]
+X2 = data.iloc[0: train_set_size, 2]
+Y = data.iloc[0: train_set_size, 3]
 
 L = 0.00001  # Learning rate
-N = len(X1)
+
 
 # initialization
 w0, w1, w2 = 0, 0, 0
@@ -25,9 +30,9 @@ while (steps > 0 and (abs(w0 - w0_new) + abs(w1 - w1_new) + abs(w2 - w2_new)) > 
 
     Y_pred = (w1 * X1) + (w2 * X2) + w0
 
-    dr_w0 = (-2 / N) * sum(Y - Y_pred)
-    dr_w1 = (-2 / N) * sum(X1 * (Y - Y_pred))
-    dr_w2 = (-2 / N) * sum(X2 * (Y - Y_pred))
+    dr_w0 = (-2 / train_set_size) * sum(Y - Y_pred)
+    dr_w1 = (-2 / train_set_size) * sum(X1 * (Y - Y_pred))
+    dr_w2 = (-2 / train_set_size) * sum(X2 * (Y - Y_pred))
 
     w0, w1, w2 = w0_new, w1_new, w2_new
 
@@ -46,3 +51,22 @@ while (steps > 0 and (abs(w0 - w0_new) + abs(w1 - w1_new) + abs(w2 - w2_new)) > 
 
 print("Final Parameters: ", w0_new, w1_new, w2_new)
 print("steps left: ", steps)
+
+# calculating error
+
+
+rms_error = 0.0
+for data_index in range(test_set_size):
+
+    index = train_set_size + data_index - 1
+    y = data.iloc[index, 3]
+    x1 = data.iloc[index, 1]
+    x2 = data.iloc[index, 2]
+
+    error = abs(y - ((w1_new * x1) + (w2_new * x2) + (w0_new)))
+    rms_error += (error * error)
+
+rms_error /= test_set_size
+rms_error = math.sqrt(rms_error)
+print("RMS Error:", rms_error)
+
