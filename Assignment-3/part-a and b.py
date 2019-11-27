6252c6e4d13bf20135a6dcb2f93b7d0f0e665138
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-11-27 19:11:29 +0530

    add comments in assignment 3.

--- Assignment-3/part-a and b.py
+++ Assignment-3/part-a and b.py
@@ -3,14 +3,17 @@ from scipy.special import gamma as gm
 import matplotlib.pyplot as plt
 import random
 
+# taking equally spaced 1000 points between 0 and 1 for graph plotting
 pdf = (np.linspace(0.0, 1.0, num=1000))
 pdf = np.sort(pdf, axis = None)
 # print(pdf)
 
+# calculating maximum likelihood (not between 0.4 and 0.6)
 uml = np.random.random()
 while (uml >= 0.4 and uml <= 0.6):
     uml = np.random.random()
 
+#generating datapoints based on uml
 data_points = ((int(uml * 160) * [1]) + (int((1 - uml) * 160) * [0]))
 random.shuffle(data_points)
 data_points = np.array(data_points) 
@@ -18,6 +21,8 @@ data_points = np.array(data_points)
 print(uml, data_points)
 
 def beta_function(a, b, u):
+    # function to calculate beta distribution value for given a, b, and u
+
     beta = (gm(a + b) / (gm(a) * gm(b))) * (u ** (a - 1)) * ((1 - u) ** (b - 1))
     return beta
 
@@ -27,12 +32,15 @@ def beta_posterior(beta, u, x):
 
 def sequential_learning(a, b):
 
+    # function to generate beta distribution sequentially
     number = 0
     for data in data_points:
         plt.clf()
         number += 1
         x_axis = []
         y_axis = []
+
+        # generating plot for given data point
         for u in pdf:
             print("data:", data)
             print("u: ", u)
@@ -55,16 +63,19 @@ def sequential_learning(a, b):
 
 def entire_dataset_learning(a, b):
 
-    
+    # function to generate beta distribution for complete dataset at once.
     x_axis = []
     y_axis = []
     for u in pdf:
+
+        # calculating final values of a and b based on dataset
         a_temp = a
         b_temp = b
         for data in data_points:
             a_temp += data
             b_temp += (1 - data)
 
+        # calculating beta function value
         posterior = beta_function(a_temp, b_temp, u)
         x_axis.append(u)
         y_axis.append(posterior)
--- Assignment-3/part_c.py
+++ Assignment-3/part_c.py
@@ -16,29 +16,33 @@ data_points = np.array(data_points)
 print(uml, data_points)
 
 def beta_function(a, b, u):
+    # function to calculate beta distribution value for given a, b, and u
+
     beta = (gm(a + b) / (gm(a) * gm(b))) * (u ** (a - 1)) * ((1 - u) ** (b - 1))
     return beta
 
 def beta_posterior(beta, u, x):
     return (beta * (u ** x) * ((1 - u) ** (1 - x)))
 
+
 def sequential_learning(a, b):
 
+    # function to generate beta distribution sequentially
     number = 0
     for data in data_points:
         plt.clf()
         number += 1
         x_axis = []
         y_axis = []
+
+        # generating plot for given data point
         for u in pdf:
             print("data:", data)
             print("u: ", u)
             prior_beta = beta_function(a, b, u)
-            posterior_beta = beta_posterior(prior_beta, u, data)
             x_axis.append(u)
             y_axis.append(prior_beta)
-        
-        prior_beta = posterior_beta
+
         a += data
         b += (1 - data)
 
@@ -46,23 +50,27 @@ def sequential_learning(a, b):
         plt.xlabel("µ: (sample used - {})".format(number))
         plt.ylabel("prior (β)")
         plt.title("µ(ML): {}".format(uml))
-        plt.savefig('prior_0_5/plot_{}.png'.format(number))
+        plt.savefig('prior/plot_{}.png'.format(number))
 
 
-sequential_learning(2, 3)
+
+# sequential_learning(2, 3)
 
 def entire_dataset_learning(a, b):
 
-    
+    # function to generate beta distribution for complete dataset at once.
     x_axis = []
     y_axis = []
     for u in pdf:
+
+        # calculating final values of a and b based on dataset
         a_temp = a
         b_temp = b
         for data in data_points:
             a_temp += data
             b_temp += (1 - data)
 
+        # calculating beta function value
         posterior = beta_function(a_temp, b_temp, u)
         x_axis.append(u)
         y_axis.append(posterior)
@@ -73,4 +81,4 @@ def entire_dataset_learning(a, b):
     plt.title("µ(ML): {}".format(uml))
     plt.show()
 
-entire_dataset_learning(2, 3)
+# entire_dataset_learning(2, 3)
